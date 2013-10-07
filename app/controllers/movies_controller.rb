387c743ef623341c0e34d2d174cdcc3de13b47fe
saasbook <saasbook@saasbook.(none)>
2013-10-06 23:23:51 -0700

    Finished all parts

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -7,16 +7,49 @@ class MoviesController < ApplicationController
   end
 
   def index
-    sort_order = params[:sort_order]
-    if sort_order == 'title'
-      @movies = Movie.all(:order => 'title ASC')
-      @title_class = 'hilite'
-    elsif sort_order == 'release_date'
-      @movies = Movie.all(:order => 'release_date ASC')
-      @release_date_class = 'hilite'
-    else
-      @movies = Movie.all
+    @all_ratings = Movie.all_ratings # retrieve all ratings
+    if !session[:ratings] # if new session, create a Hash for session
+      session[:ratings] = Hash.new(true)
+      @all_ratings.each do |movie_rating| # Hash with value 1, but no need to use values (explained in part 2)
+        session[:ratings][movie_rating] = '1'
+      end
     end
+
+    if params[:ratings] # save ratings on session
+      session[:ratings] = params[:ratings]
+    end
+
+    if params[:sort_order] # save sort order on session
+      session[:sort_order] = params[:sort_order]
+    end
+
+    params_ratings = params[:ratings]
+    session_ratings = session[:ratings]
+    params_sort_order = params[:sort_order]
+    session_sort_order = session[:sort_order]
+
+    if (!params_ratings) or ((!params_sort_order) and (session_sort_order)) # redirect to new URl with appropriate parameters
+      if flash[:notice] # we need flash[] to survive across more than a single redirect
+        flash.keep
+      end
+      redirect_to(movies_path({:ratings => session_ratings, :sort_order => session_sort_order}))
+    end
+
+    if session_sort_order == 'title' # check for sort order by title
+      @movies = Movie.find(:all, :conditions => ["rating in (?)", session_ratings.keys], :order => 'title ASC')
+      @title_class = 'hilite'
+    elsif session_sort_order == 'release_date' # check for sort order by release date
+      @movies = Movie.find(:all, :conditions => ["rating in (?)", session_ratings.keys], :order => 'release_date ASC')
+      @release_date_class = 'hilite'
+    else # check for no sort order
+      @movies = Movie.find(:all, :conditions => ["rating in (?)", session_ratings.keys])
+    end
+
+    @checked_ratings = Hash.new(true) # include checked rating boxes in the session for next time
+    @all_ratings.each do |movie_rating|
+      @checked_ratings[movie_rating] = session_ratings.include?(movie_rating)
+    end
+
   end
 
   def new
